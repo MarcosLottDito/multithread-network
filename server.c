1a5cfe337b1aa199ace688d64857dce48464f0b0
lott <marcos.lott@dito.com.br>
2024-06-04 19:24:16 -0300

    feat: updating server code to use UDP for socket communication and update client menu options

--- server.c
+++ server.c
@@ -1,74 +1,67 @@
 #include "common.h"
-#define BUFSZ 1024
+
+int clientCount = 0;
+pthread_mutex_t clientCountMutex = PTHREAD_MUTEX_INITIALIZER;
 
 struct client_data
 {
-    int clientSocket;
-    struct sockaddr_storage clientStorage;
+    int socket;
+    struct sockaddr_storage storage;
+    char movieId;
 };
 
 void *clientThread(void *data)
 {
     struct client_data *clientData = (struct client_data *)data;
+    socklen_t clientAddressLength = sizeof(clientData->storage);
 
-    int clientResponse = -1;
+    char buffer[BUFSZ];
+    memset(buffer, 0, BUFSZ);
 
-    while (clientResponse == -1)
+    for (int i = 0; i < 5; i++)
     {
-        printf("Corrida disponível:\n");
-        printf("0 - Recusar\n");
-        printf("1 - Aceitar\n");
-
-        clientResponse = menu();
-        send(clientData->clientSocket, &clientResponse, sizeof(clientResponse), 0);
-        if (clientResponse == 0)
+        switch (clientData->movieId)
         {
-            close(clientData->clientSocket);
-            printf("Aguardando solicitação.\n");
-            pthread_exit(EXIT_SUCCESS);
+        case LORD_OF_THE_RINGS:
+            getLordOfTheRings(i, buffer);
+            break;
+
+        case THE_GODFATHER:
+            getTheGodfather(i, buffer);
+            break;
+
+        case FIGHT_CLUB:
+            getFightClub(i, buffer);
+            break;
+
+        default:
+            log_exit("Invalid movie ID");
+            break;
         }
+        sendto(clientData->socket, buffer, BUFSZ, 0, (struct sockaddr *)&clientData->storage, clientAddressLength);
+
+        sleep(3);
     }
 
-    char buf[BUFSZ];
-    memset(buf, 0, BUFSZ);
+    pthread_mutex_lock(&clientCountMutex);
+    clientCount -= 1;
+    pthread_mutex_unlock(&clientCountMutex);
 
-    size_t bytesRead = recv(clientData->clientSocket, buf, BUFSZ - 1, 0);
+    memset(buffer, 0, BUFSZ);
+    close(clientData->socket);
+    free(clientData);
+    pthread_exit(EXIT_SUCCESS);
+}
 
-    // If the client closed the connection, close thread
-    if (bytesRead <= 0)
+void *serverThread(void *arg)
+{
+    while (1)
     {
-        memset(buf, 0, BUFSZ);
-        close(clientData->clientSocket);
-        printf("Aguardando solicitação.\n");
-        pthread_exit(EXIT_SUCCESS);
+        pthread_mutex_lock(&clientCountMutex);
+        printf("Clientes: %d\n", clientCount);
+        pthread_mutex_unlock(&clientCountMutex);
+        sleep(4);
     }
-
-    Coordinate serverCoordinate = {-19.9227, -43.9451};
-
-    Coordinate clientCoordinate;
-    sscanf(buf, "%lf %lf", &clientCoordinate.latitude, &clientCoordinate.longitude);
-
-    int distance = distance_between_coordinates(clientCoordinate, serverCoordinate);
-
-    while (distance > 0)
-    {
-        sprintf(buf, "Motorista a %dm\n", distance);
-        size_t bytesWritten = send(clientData->clientSocket, buf, strlen(buf) + 1, 0);
-
-        if (bytesWritten != strlen(buf) + 1)
-            log_exit("Error at send");
-
-        distance -= 400;
-
-        memset(buf, 0, BUFSZ);
-        sleep(2); // Wait for 2 seconds
-    }
-
-    printf("O motorista chegou!\n");
-
-    memset(buf, 0, BUFSZ);
-    close(clientData->clientSocket);
-    printf("Aguardando solicitação.\n");
     pthread_exit(EXIT_SUCCESS);
 }
 
@@ -81,9 +74,9 @@ int main(int argc, char **argv)
     if (0 != server_init(argv[1], argv[2], &storage))
         server_usage();
 
-    int serverSocket = socket(storage.ss_family, SOCK_STREAM, 0);
+    int serverSocket = socket(storage.ss_family, SOCK_DGRAM, 0);
     if (serverSocket == -1)
-        log_exit("Error at socket creation");
+        log_exit("Error at server socket creation");
 
     int enable = 1;
     if (0 != setsockopt(serverSocket, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int)))
@@ -93,29 +86,39 @@ int main(int argc, char **argv)
     if (0 != bind(serverSocket, address, sizeof(storage)))
         log_exit("Error at bind");
 
-    if (0 != listen(serverSocket, 10))
-        log_exit("Error at listen");
+    pthread_t serverThreadID;
+    pthread_create(&serverThreadID, NULL, serverThread, NULL);
 
-    printf("Aguardando solicitação.\n");
     while (1)
     {
         struct sockaddr_storage clientStorage;
         struct sockaddr *clientAddress = (struct sockaddr *)(&clientStorage);
         socklen_t clientAddressLength = sizeof(clientStorage);
 
-        int clientSocket = accept(serverSocket, clientAddress, &clientAddressLength);
+        int clientSocket = socket(storage.ss_family, SOCK_DGRAM, 0);
         if (clientSocket == -1)
-            log_exit("Error at accept");
+            log_exit("Error at client socket creation");
+
+        char clientChoice[CHARSZ];
+        memset(clientChoice, 0, CHARSZ);
+        int bytesRecieved = recvfrom(serverSocket, clientChoice, CHARSZ, 0, clientAddress, &clientAddressLength);
+        if (bytesRecieved == -1)
+            log_exit("Error at recvfrom");
+
+        pthread_mutex_lock(&clientCountMutex);
+        clientCount += 1;
+        pthread_mutex_unlock(&clientCountMutex);
 
         struct client_data *clientData = malloc(sizeof(*clientData));
         if (!clientData)
             log_exit("Error at malloc");
 
-        clientData->clientSocket = clientSocket;
-        memcpy(&(clientData->clientStorage), &clientStorage, sizeof(clientStorage));
+        clientData->socket = clientSocket;
+        clientData->movieId = clientChoice;
+        memcpy(&(clientData->storage), &clientStorage, sizeof(clientStorage));
 
-        pthread_t tid;
-        pthread_create(&tid, NULL, clientThread, clientData);
+        pthread_t clientThreadID;
+        pthread_create(&clientThreadID, NULL, clientThread, clientData);
     }
 
     exit(EXIT_SUCCESS);
